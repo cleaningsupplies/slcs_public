9d1654778405ad16b721770f1de5a7c2a548171b
steffi <stephanielindner@outlook.de>
2022-09-19 06:50:59 +0200

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -10,21 +10,21 @@ export default function About() {
    
       <div className={styles['text']}>
         <div className={styles['intro']}>
-            Hej, it's very nice to meet you!
+            It's very nice to meet you!
         </div>
         
         <div className={styles["description"]}>
-            My name ist Stephanie. I'm a 28 year-old junior front end developer from Hamburg, Germany. 
+            My name is Stephanie. I'm a 28 year-old junior front end developer from Hamburg, Germany. 
             <br></br>
             <br></br>
-            My coding journey began at university where I tought myself how to code in Java. 
+            My coding journey began in university where I tought myself how to code in Java. 
             About three years I worked as a research assistant programming interactive user interfaces for mobile devices in android studio.
             I hold master of science in applied cognitive and media science with a focus in computer science.
             After university I started working in IT for the city of Hamburg. 
             Ever since I missed programming and now I'm getting back on track. Programming and constantly learning new thing as a junior front end developer.
             <br></br>
             <br></br>
-            Let's get in contact ->
+            <span className={styles["link"]}>Let's get in touch &#8594;</span>
         </div>
       </div>
      
