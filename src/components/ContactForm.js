880d0c8a5b15ea71ac55139d9ac7cebdde08582d
steffi <stephanielindner@outlook.de>
2022-09-23 12:35:55 +0200

    oh noooo, it's hard

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react'
-import axios from "axios";
+import React, { useState } from 'react';
+
+
 
 export default function ContactForm() {
 
-    const [status, setStatus] = useState("Submit")
+
+
 
     //https://ciunkos.com/creating-contact-forms-with-nodemailer-and-react
 
-    function handleSubmit(e){
-        e.preventDefault();
-    }
 
+    
     
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form method='POST'>
             <div>
                 <label htmlFor='name'>Name:</label>
                 <input type="text" id="name"></input>
             </div>
             <div>
                 <label htmlFor='email'>E-Mail:</label>
-                <input type="email" id="email"></input>
+                <input type="text" id="email"></input>
             </div>
             <div>
                 <label htmlFor='message'>Message:</label>
                 <textarea type="text" id="message" required></textarea>
             </div>
-            <button type='submit'>{status}</button>
+            <button type='submit'>Snedin</button>
         </form>
     )
 }
