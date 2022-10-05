68fde23deee22900f8601767ee9d6693450a58c5
steffi <stephanielindner@outlook.de>
2022-10-05 21:43:54 +0200

    trying to select from css module class

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,34 +4,65 @@ import About from "../components/About";
 import Contact from "../components/Contact";
 import Projects from '../components/Projects';
 import styles from "../css/navigation.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default function Navigation() {
+
+    const tet = ".navigation_navbar__+SmCa";
+
+    function openNavMenu() {
+        
+        let x = document.getElementById("myTopnav");
+        //make navigation responsive (add responsive to class)
+        
+        //let y = document.querySelector(x.className);
+        
+        // y.style.color = "red"
+
+        //https://codesandbox.io/s/flamboyant-sun-flfpj?file=/src/App.js
+        //https://stackoverflow.com/questions/69617831/how-do-i-select-my-css-module-class-using-document-queryselector
+
+        console.log(x.classList, document.querySelector(`.${styles.scrollValue}`));
+        // if (x.className === "topnav") {
+        //   x.className += " responsive";
+        // } else {
+        //   x.className = "topnav";
+        // }
+      }
+
   return (
     <>
-      <BrowserRouter className={styles["navigation"]}>
-          <div className={styles['navbar']}>
-              <div className={styles['navItem']}>
-                  <NavLink className={({ isActive }) => (isActive ? `${styles['NavLink']} ${styles['active']}` : `${styles['NavLink']} ${styles['inactive']}`)} to="/">
-                      Projects
-                  </NavLink>
-              </div>
-              <div className={styles['navItem']}>
-                  <NavLink className={({ isActive }) => (isActive ? `${styles['NavLink']} ${styles['active']}` : `${styles['NavLink']} ${styles['inactive']}`)} to="/about">
-                      About
-                  </NavLink>
-              </div>
-              <div className={`${styles['navItem']} ${styles['contact']}`}>
-                  <NavLink className={({ isActive }) => (isActive ? `${styles['NavLink']} ${styles['active']}` : `${styles['NavLink']} ${styles['inactive']}`)} to="/contact">
-                      Contact
-                  </NavLink>
-              </div>
-          </div>
-          <Routes>
-              <Route exact path="/" element={<Projects />} />
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/contact" element={<Contact />} />
-          </Routes>
-      </BrowserRouter>
-  </>
+        <BrowserRouter className={styles["navigation"]}>
+        <h1 className={styles.scrollValue}>0</h1>
+            <div id="myTopnav" className={styles['navbar']}>
+                <div className={styles['navItem']}>
+                    <NavLink className={({ isActive }) => (isActive ? `${styles['NavLink']} ${styles['active']}` : `${styles['NavLink']} ${styles['inactive']}`)} to="/">
+                        Projects
+                    </NavLink>
+                </div>
+                <div className={styles['navItem']}>
+                    <NavLink className={({ isActive }) => (isActive ? `${styles['NavLink']} ${styles['active']}` : `${styles['NavLink']} ${styles['inactive']}`)} to="/about">
+                        About
+                    </NavLink>
+                </div>
+                <div className={`${styles['navItem']} ${styles['contact']}`}>
+                    <NavLink className={({ isActive }) => (isActive ? `${styles['NavLink']} ${styles['active']}` : `${styles['NavLink']} ${styles['inactive']}`)} to="/contact">
+                        Contact
+                    </NavLink>
+                </div>
+                <div href="javascript:void(0);" className={`${styles['navItem']} ${styles['menu']}`} onClick={openNavMenu}>
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+            </div>
+            <Routes>
+                <Route exact path="/" element={<Projects />} />
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
+    </>
   )
 }
