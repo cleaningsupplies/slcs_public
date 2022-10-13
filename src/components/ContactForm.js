0ddb673a362233f87499d406682d2d2651b4c072
steffi <stephanielindner@outlook.de>
2022-10-13 20:19:22 +0200

    test

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -59,10 +59,10 @@ export default function ContactForm() {
               <button className={styles["submit"]} type="Submit">{status}</button>
             </div>
           </form> */}
-          <form id="form" className={styles["form"]} name="contact/sent" action="/contact/sent" method="post">
-            <input type="hidden" name="form-name" value="contact/sent"></input>
+          <form id="form" className={styles["form"]} name="contact" action="/contact" method="post">
+            <input type="hidden" name="form-name" value="contact"></input>
             <label id="n" htmlFor='name'>Name</label>
-            <input id="name" name="name" className={styles["name"]} placeholder="Name" type="text" required></input>
+            <input id="name" name="name" className={styles["name"]} placeholder="NameOLL" type="text" required></input>
             <label id="e" htmlFor='email'>E-Mail address</label>
             <input id="email" name="email" className={styles["email"]} placeholder="E-Mail address" type="email" required></input>
             <label id="m" htmlFor='message'>Message</label>
