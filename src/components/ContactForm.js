cc41f6ac292f3d678b1a5762366e397ac79a545b
steffi <stephanielindner@outlook.de>
2022-10-13 19:31:56 +0200

    upload and switch to other repo

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -47,14 +47,27 @@ export default function ContactForm() {
     return (
       <div className={styles["container"]}>
         <div className={styles["formContainer"]}>
-        
-          <form id="form" className={styles["form"]} onSubmit={handleSubmit}>
+          {/* <form id="form" className={styles["form"]} onSubmit={handleSubmit}>
             <label id="n" htmlFor='name'>Name</label>
             <input id="name" className={styles["name"]} placeholder="Name" type="text" tabIndex="12" required></input>
             <label id="e" htmlFor='email'>E-Mail address</label>
             <input id="email" className={styles["email"]} placeholder="E-Mail address" type="email" tabIndex="13" required></input>
             <label id="m" htmlFor='message'>Message</label>
             <textarea id="message" className={styles["message"]} placeholder="Message" type="text" tabIndex="14" required></textarea>
+            <div className={styles["button_container"]}>
+              <div id="sent" className={styles["sent"]}>{sent}</div>
+              <button className={styles["submit"]} type="Submit">{status}</button>
+            </div>
+          </form> */}
+
+          <form id="form" className={styles["form"]} name="contact/sent" action="/contact/sent" method="post">
+            <input type="hidden" name="form-name" value="contact/sent"></input>
+            <label id="n" htmlFor='name'>Name</label>
+            <input id="name" name="name" className={styles["name"]} placeholder="Name" type="text" required></input>
+            <label id="e" htmlFor='email'>E-Mail address</label>
+            <input id="email" name="email" className={styles["email"]} placeholder="E-Mail address" type="email" required></input>
+            <label id="m" htmlFor='message'>Message</label>
+            <textarea id="message" className={styles["message"]} name="message" placeholder="Message" type="text" required></textarea>
             <div className={styles["button_container"]}>
               <div id="sent" className={styles["sent"]}>{sent}</div>
               <button className={styles["submit"]} type="Submit">{status}</button>
