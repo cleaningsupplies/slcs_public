d4bc586b5187981cc441d83657d66b3385625e54
steffi <stephanielindner@outlook.de>
2022-11-11 13:37:17 +0100

    ok. let's see it live

--- src/components/About.js
+++ src/components/About.js
@@ -14,14 +14,14 @@ export default function About() {
               <br></br>
               <br></br>
               <br></br>
-              My name is Stephanie. I'm a 28 year-old junior front end developer from Hamburg, Germany. 
+              My name is Stephanie. I'm a 28 year-old junior frontend developer from Hamburg, Germany. 
               <br></br>
               <br></br>
               My coding journey began in university where I taught myself how to code in Java. 
               About three years I worked as a research assistant programming interactive user interfaces for mobile devices in android studio.
               I hold master of science in applied cognitive and media science with a focus in computer science.
               After university I started working in IT for the city of Hamburg. 
-              Ever since, I missed programming and now I'm back on track programming and constantly learning new things as a junior front end developer.
+              Ever since, I missed programming and now I'm back on track programming and constantly learning new things as a junior frontend developer.
 
               {/* Ich bin kreative Junior Frontend Entwicklerin mit umfangreichen Kenntnissen in Grafik- und UX/UI-Design. In meinen bisherigen Projekten habe ich aber auch Erfahrungen im Backend gesammelt. Ich programmiere leidenschaftlich gerne und gehe besonders in Themen wie Design, Responsiveness und Accessibility auf. */}
 
