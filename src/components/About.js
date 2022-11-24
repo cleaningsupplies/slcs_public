1ec4457a06b55083d1382c9549099227e793787b
steffi <stephanielindner@outlook.de>
2022-11-24 19:53:00 +0100

    text edit

--- src/components/About.js
+++ src/components/About.js
@@ -14,14 +14,18 @@ export default function About() {
               <br></br>
               <br></br>
               <br></br>
-              My name is Stephanie. I'm a 28 year-old frontend developer from Hamburg, Germany. 
-              <br></br>
-              <br></br>
-              My coding journey began in university where I taught myself how to code in Java. 
+              
+              {/* My coding journey began in university where I taught myself how to code in Java. 
               About three years I worked as a research assistant programming interactive user interfaces for mobile devices in android studio.
               I hold master of science in applied cognitive and media science with a focus in computer science.
-              After university I started working in IT for the city of Hamburg. 
-              Ever since, I missed programming and now I'm back on track programming and constantly learning new things as a frontend developer.
+               programming and constantly learning new things as a frontend developer. */}
+
+               My name is Stephanie. I'm an enthusiastic frontend developer with extensive background-knowledge in graphic as well as UX/UI design. 
+               My passion is coding and learning new skills. I already gained a great number of experiences through past projects 
+               as frontend developer as well as UX/UI designer for a smartphone application in the context of e-health. 
+               Furthermore, I have been active as backend developer in numerous distinctive settings. 
+               Momentarily I'm working on private projects focusing on frontend tools such as HTML, CSS, JavaScript and React. 
+               I am very dedicated to my work and expand my knowledge day by day.
 
               {/* Ich bin kreative Junior Frontend Entwicklerin mit umfangreichen Kenntnissen in Grafik- und UX/UI-Design. In meinen bisherigen Projekten habe ich aber auch Erfahrungen im Backend gesammelt. Ich programmiere leidenschaftlich gerne und gehe besonders in Themen wie Design, Responsiveness und Accessibility auf. */}
 
